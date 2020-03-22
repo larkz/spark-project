9ee4c2df16d093f0523eb14c65993fef8710167a
Larkin <liu.larkin@gmail.com>
2020-03-22 20:43:24 +0100

    updates for Gaurang

--- src/main/scala/etl/DataProcessing.scala
+++ src/main/scala/etl/DataProcessing.scala
@@ -2,15 +2,28 @@ package etl
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-
 object DataProcessing {
     println("larkin")
+    val spark = SparkSession.builder().getOrCreate()
 
     def getParquet(parquetPath: String): DataFrame = {
         val spark = SparkSession.builder().getOrCreate()
         println("read parquet!")
         spark.read.parquet(parquetPath)
     }
+
+    def readCSV(csvPath: String): DataFrame = {
+        println("read csv!")
+        val csvPath = "/Users/eric/Dropbox/SharpestMinds/Gaurang/Mentorship/instacart_prediction/data/order_products__prior.csv"
+        val csvDf = spark.read.format("csv").option(
+            "header", "true").option(
+            "inferSchema", "true").load(
+            csvPath
+        )
+        csvDf
+    }
+
+    // "⁨/Users/eric/Dropbox/SharpestMinds/Gaurang/Mentorship/instacart_prediction/data/order_products__prior.csv"
 
     def writeToCSV(df: DataFrame, fileName: DataFrame): Unit = {
         val spark = SparkSession.builder().getOrCreate()
